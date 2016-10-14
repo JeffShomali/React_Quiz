e92296b7bd2660d66fb935e11de43061bf45cf80
Jeff Shomali <jeffshomali@gmail.com>
2016-10-13 19:19:12 -0700

    Adding data into APP.jsx
    Adding array of questions  and answers.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,99 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import QuestionList from './quiz/QuestionList.jsx';
 
 class App extends Component{
+     constructor(props){
+          super(props);
+          this.state = {
+               questions:[
+                    {
+                         id: 1,
+                         text: 'What is your name?',
+                         choices:[
+                              {
+                                   id: 'a',
+                                   text: 'Jeff'
+                              },
+                              {
+                                   id: 'b',
+                                   text: 'Sam'
+                              },
+                              {
+                                   id: 'c',
+                                   text: 'Dave'
+                              }
+                         ],
+                         correct: 'b'
+                    },
+                    {
+                         id: 2,
+                         text: 'What is your GF name?',
+                         choices:[
+                              {
+                                   id: 'a',
+                                   text: 'Sara'
+                              },
+                              {
+                                   id: 'b',
+                                   text: 'Maha'
+                              },
+                              {
+                                   id: 'c',
+                                   text: 'Parisa'
+                              }
+                         ],
+                         correct: 'c'
+                    },
+                    {
+                         id: 3,
+                         text: 'What is your best friend name?',
+                         choices:[
+                              {
+                                   id: 'a',
+                                   text: 'Habib'
+                              },
+                              {
+                                   id: 'b',
+                                   text: 'Alex'
+                              },
+                              {
+                                   id: 'c',
+                                   text: 'Mami'
+                              }
+                         ],
+                         correct: 'a'
+                    },
+                    {
+                         id: 4,
+                         text: 'What is your favorite car?',
+                         choices:[
+                              {
+                                   id: 'a',
+                                   text: 'BMW'
+                              },
+                              {
+                                   id: 'b',
+                                   text: 'Benz'
+                              },
+                              {
+                                   id: 'c',
+                                   text: 'Tesla'
+                              }
+                         ],
+                         correct: 'b'
+                    }
+               ],
+               score: 0,
+               current: 1
+          }
+     }
+
 
      render(){
           return (
                <div>
-                    APP
+                    <QuestionList {...this.state} />
                </div>
           )
      }
